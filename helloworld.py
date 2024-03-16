78abb9104b3d6a7a746b9eee8b2bcdfeece3f0ee
xinnanwei-pvcc <160061380+xinnanwei-pvcc@users.noreply.github.com>
2024-03-16 00:34:46 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+#Name: Xinnan Wei
+#Program Purpose: Hello World in Python
+
+print('I love Python!')
+print("\n\t\\Hello\\ \n\t\tWorld!")
+
+#Python can do arithmetic too.
+aiden = 20
+albert = 19
+drstevens = 68
+sum_ages = aiden + albert + drstevens
+ave = sum_ages / 3
+print("The average age is:", ave)
+
+
+#Next year:
+aiden +=10
+albert +=10
+drstevens +=10
+sum_ages = aiden + albert + drstevens
+ave = sum_ages / 3
+print("Next year, the average age is:", ave)
